de7ac8f38a3c9414fd6dc083a2ce3bd11dc991f2
Ferran Petit <fpmartiwork@gmail.com>
2022-07-28 19:46:52 +0200

    Video number 14

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,11 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    baseUrl: 'https://demoqa.com',
+    excludeSpecPattern: '**/examples/**',
+    viewportWidth: 1920,
+    viewportHeight: 1080,
+    setupNodeEvents(on, config) {},
   },
 });
